b29af91f31456205052660ee068d77056823998f
Philippe Matray <phmatray@gmail.com>
2023-12-14 17:46:53 +0100

    write some tests for Transition.cs

--- BlazorTailwind/Components/UI/Transition.cs
+++ BlazorTailwind/Components/UI/Transition.cs
@@ -5,77 +5,177 @@ namespace BlazorTailwind.Components.UI;
 
 public class Transition : ComponentBase
 {
+    /// <summary>
+    /// Whether the children should be shown or hidden.
+    /// </summary>
     [Parameter] public bool Show { get; set; }
-    [Parameter] public bool Appear { get; set; } = false;
 
-    [Parameter] public string? Enter { get; set; }
-    [Parameter] public string? EnterFrom { get; set; }
-    [Parameter] public string? EnterTo { get; set; }
-    [Parameter] public string? Leave { get; set; }
-    [Parameter] public string? LeaveFrom { get; set; }
-    [Parameter] public string? LeaveTo { get; set; }
-    [Parameter] public RenderFragment? ChildContent { get; set; }
-
-    [Parameter] public Action? BeforeEnter { get; set; }
-    [Parameter] public Action? AfterEnter { get; set; }
-    [Parameter] public Action? BeforeLeave { get; set; }
-    [Parameter] public Action? AfterLeave { get; set; }
-
+    /// <summary>
+    /// The element to render in place of the Transition itself.
+    /// </summary>
     [Parameter] public string As { get; set; } = "div";
+    
+    /// <summary>
+    /// Whether the transition should run on initial mount.
+    /// </summary>
+    [Parameter] public bool Appear { get; set; } = false;
+    
+    /// <summary>
+    /// Whether the element should be unmounted or hidden based on the show state.
+    /// </summary>
+    [Parameter] public bool Unmount { get; set; } = true;
+    
+    /// <summary>
+    /// Whether the element should be unmounted or hidden based on the show state.
+    /// </summary>
+    [Parameter] public string? Enter { get; set; }
+    
+    /// <summary>
+    /// Classes to add to the transitioning element before the enter phase starts.
+    /// </summary>
+    [Parameter] public string? EnterFrom { get; set; }
+    
+    /// <summary>
+    /// Classes to add to the transitioning element immediately after the enter phase starts.
+    /// </summary>
+    [Parameter] public string? EnterTo { get; set; }
+    
+    /// <summary>
+    /// Classes to add to the transitioning element once the transition is done. These classes will persist after that, until it's time to leave.
+    /// </summary>
+    [Parameter] public string? Entered { get; set; }
+    
+    /// <summary>
+    /// Classes to add to the transitioning element during the entire leave phase.
+    /// </summary>
+    [Parameter] public string? Leave { get; set; }
+    
+    /// <summary>
+    /// Classes to add to the transitioning element before the leave phase starts.
+    /// </summary>
+    [Parameter] public string? LeaveFrom { get; set; }
+    
+    /// <summary>
+    /// Classes to add to the transitioning element immediately after the leave phase starts.
+    /// </summary>
+    [Parameter] public string? LeaveTo { get; set; }
+
+    /// <summary>
+    /// Callback which is called before we start the enter transition.
+    /// </summary>
+    [Parameter] public Action BeforeEnter { get; set; } = () => { };
+    
+    /// <summary>
+    /// Callback which is called after we finished the enter transition.
+    /// </summary>
+    [Parameter] public Action AfterEnter { get; set; } = () => { };
+    
+    /// <summary>
+    /// Callback which is called before we start the leave transition.
+    /// </summary>
+    [Parameter] public Action BeforeLeave { get; set; } = () => { };
+    
+    /// <summary>
+    /// Callback which is called after we finished the leave transition.
+    /// </summary>
+    [Parameter] public Action? AfterLeave { get; set; }
+    
+    [Parameter] public RenderFragment? ChildContent { get; set; }
 
     [Parameter(CaptureUnmatchedValues = true)]
     public IDictionary<string, object>? AdditionalAttributes { get; set; }
+    
+    private bool IsEntering { get; set; }
+    private bool IsLeaving { get; set; }
 
-    private bool isEntering;
-    private bool isLeaving;
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        SetInitialAnimationState();
+    }
 
     protected override void OnParametersSet()
     {
-        if (Show)
+        base.OnParametersSet();
+        UpdateAnimationState();
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (IsEntering)
         {
-            isEntering = true;
-            isLeaving = false;
-            BeforeEnter?.Invoke();
+            IsEntering = false;
+            AfterEnter?.Invoke();
         }
-        else
+        else if (IsLeaving)
         {
-            isLeaving = true;
-            isEntering = false;
-            BeforeLeave?.Invoke();
+            IsLeaving = false;
+            AfterLeave?.Invoke();
         }
     }
-    
+
     protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
         builder.OpenElement(0, As);
-        builder.AddAttribute(1, "class", GetCurrentCssClass());
-        builder.AddContent(2, ChildContent);
+        
+        var classNames = GetCurrentCssClass();
+        if (!string.IsNullOrWhiteSpace(classNames))
+        {
+            builder.AddAttribute(1, "class", classNames);
+        }
+        
+        if (AdditionalAttributes != null)
+        {
+            foreach (var kvp in AdditionalAttributes)
+            {
+                builder.AddAttribute(2, kvp.Key, kvp.Value);
+            }
+        }
+        
+        builder.AddContent(3, ChildContent);
         builder.CloseElement();
+    }
+
+    private void SetInitialAnimationState()
+    {
+        if (Show)
+        {
+            IsEntering = Appear;
+            IsLeaving = false;
+        }
+        else
+        {
+            IsEntering = false;
+            IsLeaving = false;
+        }
+    }
+
+
+    private void UpdateAnimationState()
+    {
+        if (Show && !IsEntering)
+        {
+            IsEntering = true;
+            IsLeaving = false;
+            BeforeEnter?.Invoke();
+        }
+        else if (!Show && !IsLeaving)
+        {
+            IsEntering = false;
+            IsLeaving = true;
+            BeforeLeave?.Invoke();
+        }
     }
 
     private string GetCurrentCssClass()
     {
-        var enterClasses = isEntering ? $"{Enter} {EnterFrom}" : EnterTo;
-        var leaveClasses = isLeaving ? $"{Leave} {LeaveFrom}" : LeaveTo;
+        var enterClasses = IsEntering ? $"{Enter} {EnterFrom}".Trim() : EnterTo;
+        var leaveClasses = IsLeaving ? $"{Leave} {LeaveFrom}".Trim() : LeaveTo;
         var currentClasses = Show ? enterClasses : leaveClasses;
 
         return AdditionalAttributes?
             .TryGetValue("class", out var attribute) is true
             ? $"{attribute} {currentClasses}"
             : currentClasses ?? string.Empty;
-    }
-
-    protected override void OnAfterRender(bool firstRender)
-    {
-        if (isEntering)
-        {
-            isEntering = false;
-            AfterEnter?.Invoke();
-        }
-        else if (isLeaving)
-        {
-            isLeaving = false;
-            AfterLeave?.Invoke();
-        }
     }
 }
